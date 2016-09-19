231138b39b55425adde8b79a78542be732fbd503
Connor Gray <csgray1414@gmail.com>
2016-09-18 23:58:44 -0400

    implemented kanji search. outputs in console for now

--- js/kanjisheets.js
+++ js/kanjisheets.js
@@ -8,6 +8,26 @@ xhttp.open("GET", "../kanjidic2.xml", true);
 xhttp.send();
 
 function readXML(xml) {
-    var xmlDoc = xml.responseXML;
-    console.log(xmlDoc.getElementsByTagName("c")[0]);
+    xmlDoc = xml.responseXML.getElementsByTagName("c");
+}
+
+function textChange(input) {
+	value = input.target.value.toLowerCase();
+	search(value);
+}
+
+
+function search(text) {
+	var match = [];
+	for (var i=0; i < xmlDoc.length; i++) {
+		var entry = xmlDoc[i].innerHTML;
+		var entry_length = entry.length;
+		entry = entry.replace(text,"");
+		var reduction = entry_length - entry.length;
+		if (reduction == text.length) {
+			match.push(xmlDoc[i]);
+			console.log(xmlDoc[i].innerHTML);
+		}
+	}
+	console.log(match);
 }

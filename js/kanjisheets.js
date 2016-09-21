f1c0e03b15c0328919bd9a862d568b98c46879fc
Connor Gray <csgray1414@gmail.com>
2016-09-20 20:52:58 -0400

    hooked up kanji search to page.

--- js/kanjisheets.js
+++ js/kanjisheets.js
@@ -13,7 +13,9 @@ function readXML(xml) {
 
 function textChange(input) {
 	value = input.target.value.toLowerCase();
-	search(value);
+	console.log(value);
+	if (value.length > 1)
+		search(value);
 }
 
 
@@ -25,9 +27,39 @@ function search(text) {
 		entry = entry.replace(text,"");
 		var reduction = entry_length - entry.length;
 		if (reduction == text.length) {
-			match.push(xmlDoc[i]);
-			console.log(xmlDoc[i].innerHTML);
+			match.push(xmlDoc[i].innerHTML.split("~"));
 		}
 	}
-	console.log(match);
+	arrange(match);
+}
+
+function arrange(match) {
+	match = match.sort(function(a, b) {
+		if (parseInt(a[1]) < parseInt(b[1])) return -1;
+		if (parseInt(a[1]) > parseInt(b[1])) return 1;
+		return 0;
+	});
+	output(match);
+}
+
+function output(match) {
+	var markers_used = [];
+	var characters = document.getElementById("characters");
+	characters.innerHTML = "";
+	for (var i=0; i < match.length; i++) {
+		if (markers_used.indexOf(match[i][1]) == -1) {
+			markers_used.push(match[i][1]);
+			characters.innerHTML += character_format(match[i][1], true);
+		}
+		characters.innerHTML += character_format(match[i][0], false);
+	}
+}
+
+function character_format(character, marker) {
+	if (marker === true)
+		marker = " marker";
+	else
+		marker = "";
+	result = '<div class="col-md-3"><div class="character' + marker + '"><div class="vtable"><div class="vtable-cell">' + character + '</div></div></div></div>'
+	return result;
 }

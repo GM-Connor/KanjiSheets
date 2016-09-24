96f3da7a4cc9286bb9b3a50da0e8cb1515fd3266
Connor Gray <csgray1414@gmail.com>
2016-09-23 20:32:40 -0400

    hookedup kanji search to kanji selected.

--- js/kanjisheets.js
+++ js/kanjisheets.js
@@ -20,8 +20,8 @@ function textChange(input) {
 	/*Triggered by onkeyup attr for #characters*/
 	//Converts input to lowercase to avoid disqualified results due to case mismatch
 	value = input.target.value.toLowerCase();
-	//Requires text input of atleast two characters to avoid needlessly large search results
-	if (value.length > 1) {
+	//Requires text input of atleast three characters to avoid needlessly large search results
+	if (value.length > 2) {
 		//Expands results area to expose spinning gif until overwritten with results
 		$('#search-results').collapse('show');
 		determineHeight();
@@ -102,6 +102,63 @@ function character_format(character, marker) {
  * Adding kanji to selected grid (from search results to grid. or directly to grid)
  */
 
-function kanjiadd(kanji) {
-	var grid = document.getElementById("selectedkanji");
+function buttonClick(event) {
+	/*Triggered by "plus" button on kanji search form*/
+	//Gets what's inside of kanji search input field, removes surrounding white space, and splits into array of charcters
+	var val = $('#search-expand input')[0].value.trim().split("");
+	//Calls kanjiAdd for each character
+	for (var i=0; i < val.length; i++) {
+		kanjiAdd(val[i]);
+	}
 }
+function kanjiAdd(kanji) {
+	/*Triggered by buttonClick(), addFromNew()*/
+	//Max kanji allowed selected
+	var limit = 10;
+	//Number of current kanji selected
+	var amountSelected = $('#selectedkanji > div').length - $('#selectedkanji > div.new').length;
+	//Makes sure character is character, and that the number of selected kanji haven't hit max allowed
+	if ((kanji != "") && amountSelected < limit) {
+		var grid = document.getElementById("selectedkanji");
+		var newBlock = $('#selectedkanji .new');
+		newBlock.remove();
+		//Appends the kanji to grid
+		grid.innerHTML += gridFormat(kanji,false);
+		//Appends "new" block to grid more kanji are allowed after this one
+		if (amountSelected != limit - 1) {
+			grid.innerHTML += gridFormat("",true);
+		}
+	}
+}
+function gridFormat(kanji, isNew) {
+	/*Triggered by kanjiAdd()*/
+	//Returns HTML for entry based on if a kanji or "new" block
+	if (isNew) {
+		return '<div class="col-md-10 alert new"><div class="character-selected"><div class="vtable"><div class="vtable-cell"><input type="text" placeholder="+" onfocusout="addFromNew(this.value);"></div></div></div></div>';
+	}
+	else {
+		return '<div class="col-md-10 alert"><div class="character-selected"><div class="vtable"><div class="vtable-cell">' + kanji + '</div><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div></div>';
+	}
+}
+$('#selectedkanji').keyup(function(event){
+	if (event.keyCode == 13) {
+		addFromNew($('#selectedkanji .new input')[0].value);
+	}
+});
+function addFromNew(text) {
+	/*Triggered by onfocusout of "new" block, and/or enter key pressed within grid*/
+	//Gets value of "new" block's input, and make sure it isn't blank
+	if ( text && text.length > 0) {
+		//Gets first character of input. Sends through kanjiAdd();
+		var val = text.trim().split("")[0];
+		kanjiAdd(val);
+	}
+}
+
+
+
+
+
+/*
+ * Selected kanji to SVG hookup
+ */
